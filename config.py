1144c4825c425779059a14a8daf92676dd04fc54
Brandon <31549535+Bgreen23@users.noreply.github.com>
2018-12-12 07:55:06 -0500

    updated db

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ port = int(os.environ.get('PORT', 5000))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xa9w\xb9{\xbbC;\x0bQ>\xd8\xfb \x03o\x90'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://nckbhqxhruvwcz:efb53cfe3d5fc544849082317adf699dacde344d42fed6fd5a427ba8f3d2687b@ec2-54-235-193-0.compute-1.amazonaws.com:5432/d8gbnciq0f9fpb') or \
+    SQLALCHEMY_DATABASE_URI = app.config('postgres://nckbhqxhruvwcz:efb53cfe3d5fc544849082317adf699dacde344d42fed6fd5a427ba8f3d2687b@ec2-54-235-193-0.compute-1.amazonaws.com:5432/d8gbnciq0f9fpb') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
